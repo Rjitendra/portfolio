a159bb7adeaa6606ff181dc8709fa6ed88f884c0
Jitendra Behera <jb.jitu@live.com>
2026-03-07 21:52:39 +0530

    t

--- src/app/features/components/home/home.component.ts
+++ src/app/features/components/home/home.component.ts
@@ -1,25 +1,8 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ng-home',
-  imports: [
-    RouterModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatMenuModule,
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
