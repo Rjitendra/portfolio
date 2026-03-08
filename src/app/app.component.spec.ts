0de04d67680a0e948fafbf4d1a9b9c2508fbd2f6
Jitendra Behera <jb.jitu@live.com>
2026-03-08 08:38:34 +0530

    jh

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it("should have the 'nglearn' title", () => {
+  it("should have the 'NG-Learn' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nglearn');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nglearn');
+    expect(app.title).toEqual('NG-Learn');
   });
 });
--- src/app/features/components/reactive-form/reactive-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ng-reactive-form',
-  imports: [],
-  templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.scss',
-})
-export class ReactiveFormComponent {}
